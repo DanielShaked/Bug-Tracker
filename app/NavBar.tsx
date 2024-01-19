369c76207c3bc32f4fd7a1fb443358c8a8a64d17
DanielShaked <89588973+DanielShaked@users.noreply.github.com>
2024-01-19 12:52:14 +0200

    add classnames lib support

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,9 +1,14 @@
+'use client'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { AiOutlineBug } from "react-icons/ai";
-
+import classnames from 'classnames'
 
 const NavBar = () => {
+    const pathName = usePathname()
+    // console.log('pathName', pathName)
+
     const links = [
         { label: 'Dashboard', href: '/' },
         { label: 'Issues', href: '/issues' }
@@ -13,10 +18,19 @@ const NavBar = () => {
         <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
             <Link href='/'><AiOutlineBug /></Link>
             <ul className='flex space-x-6'>
-                {links.map(link => <Link key={link.href} className='text-zinc-500 hover:text-zinc-800 transition-colors' href={link.href}>{link.label}</Link>)}
+                {links.map(link => <Link
+                    key={link.href}
+                    className={classnames({
+                        'text-zinc-900': link.href === pathName,
+                        'text-zinc-500': link.href !== pathName,
+                        'hover:text-zinc-800 transition-colors': true
+                    })}
+                    href={link.href}>
+                    {link.label}
+                </Link>)}
 
             </ul>
-        </nav>
+        </nav >
     )
 }
 
