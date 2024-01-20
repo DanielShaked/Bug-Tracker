d0dee7ebcfe4d8371b7ec6a13a0b1e2ad3b8f18f
DanielShaked <89588973+DanielShaked@users.noreply.github.com>
2024-01-20 22:54:41 +0200

    add markdown editor

--- app/bugs/new/page.tsx
+++ app/bugs/new/page.tsx
@@ -1,16 +1,16 @@
 'use client'
-import { Button, TextArea, TextField } from '@radix-ui/themes'
-import React from 'react'
+import { Button, TextField } from '@radix-ui/themes'
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const NewBugPage = () => {
     return (
         <div className='max-w-xl space-y-3'>
             <TextField.Root>
                 <TextField.Input placeholder='Title'>
-
                 </TextField.Input>
             </TextField.Root>
-            <TextArea placeholder='Description'></TextArea>
+            <SimpleMDE placeholder='Description'></SimpleMDE>
             <Button>Submit New Bug</Button>
         </div>
     )
