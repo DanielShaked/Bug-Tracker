f3a3c8dffd7d7f0615e608eddc98cc82bdfc3858
DanielShaked <89588973+DanielShaked@users.noreply.github.com>
2024-01-20 22:29:43 +0200

    add newBug route and cmp

---
+++ app/bugs/new/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+import { Button, TextArea, TextField } from '@radix-ui/themes'
+import React from 'react'
+
+const NewBugPage = () => {
+    return (
+        <div className='max-w-xl space-y-3'>
+            <TextField.Root>
+                <TextField.Input placeholder='Title'>
+
+                </TextField.Input>
+            </TextField.Root>
+            <TextArea placeholder='Description'></TextArea>
+            <Button>Submit New Bug</Button>
+        </div>
+    )
+}
+
+export default NewBugPage
--- app/bugs/page.tsx
+++ app/bugs/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Button } from '@radix-ui/themes'
+import Link from 'next/link'
 const BugsPage = () => {
     return (
         <div>
             <h1>Bugs page</h1>
-            <Button>Add Bug</Button>
+            <Button>
+                <Link href='/bugs/new'>Add Bug</Link>
+            </Button>
         </div>
     )
 }
