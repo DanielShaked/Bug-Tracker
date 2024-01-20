ed0f656a6bf6cbed6b4f4e16118b6d4450d2dc4e
DanielShaked <89588973+DanielShaked@users.noreply.github.com>
2024-01-20 22:17:30 +0200

    setup redix ui

---
+++ app/bugs/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '@radix-ui/themes'
+const BugsPage = () => {
+    return (
+        <div>
+            <h1>Bugs page</h1>
+            <Button>Add Bug</Button>
+        </div>
+    )
+}
+
+export default BugsPage
