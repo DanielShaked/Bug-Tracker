acf4d0565c98725edb5d875f25c36412dfe8a940
DanielShaked <89588973+DanielShaked@users.noreply.github.com>
2024-01-21 22:38:07 +0200

    handle error

--- app/api/bugs/route.ts
+++ app/api/bugs/route.ts
@@ -10,7 +10,7 @@ export async function POST(request: NextRequest) {
     const body = await request.json()
     const validation = createBugSchema.safeParse(body)
     if (!validation.success)
-        return NextResponse.json(validation.error.errors), { status: 400 }
+        return NextResponse.json(validation.error.format()), { status: 400 }
 
 
     const newBug = await prisma.bug.create({
--- app/bugs/new/page.tsx
+++ app/bugs/new/page.tsx
@@ -1,10 +1,12 @@
 'use client'
-import { Button, TextField } from '@radix-ui/themes'
+import { Button, Callout, TextField } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios'
 import { useRouter } from 'next/navigation';
+
 import "easymde/dist/easymde.min.css";
+import { useState } from 'react';
 
 interface BugForm {
     title: string
@@ -14,26 +16,37 @@ interface BugForm {
 const NewBugPage = () => {
     const router = useRouter()
     const { register, control, handleSubmit } = useForm<BugForm>()
+    const [error, setError] = useState('')
     return (
-        <form
-            className='max-w-xl space-y-3'
-            onSubmit={handleSubmit(async (data) => {
-                const res = await axios.post('/api/bugs', data)
-                router.push('/bugs')
-            })}>
-            <TextField.Root>
-                <TextField.Input placeholder='Title' {...register('title')}>
-                </TextField.Input>
-            </TextField.Root>
-            <Controller
-                name="description"
-                control={control}
-                render={({ field }) => (
-                    <SimpleMDE placeholder="Description" {...field} />
-                )}
-            />
-            <Button>Submit New Bug</Button>
-        </form>
+        <div className='max-w-xl'>
+            {error && <Callout.Root className='mb-5' color='red'>
+                <Callout.Text>{error}</Callout.Text>
+            </Callout.Root>}
+            <form
+                className='max-w-xl space-y-3'
+                onSubmit={handleSubmit(async (data) => {
+                    try {
+                        const res = await axios.post('/api/bugs', data)
+                        router.push('/bugs')
+                    } catch (error) {
+                        setError('An unexpected error was occuerd')
+                        console.error(error)
+                    }
+                })}>
+                <TextField.Root>
+                    <TextField.Input placeholder='Title' {...register('title')}>
+                    </TextField.Input>
+                </TextField.Root>
+                <Controller
+                    name="description"
+                    control={control}
+                    render={({ field }) => (
+                        <SimpleMDE placeholder="Description" {...field} />
+                    )}
+                />
+                <Button>Submit New Bug</Button>
+            </form>
+        </div>
     )
 }
 
