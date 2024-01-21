c837a293bbc17352d4b6c65eb4389099e0de6ba3
DanielShaked <89588973+DanielShaked@users.noreply.github.com>
2024-01-22 00:04:09 +0200

    show bugs

--- app/bugs/page.tsx
+++ app/bugs/page.tsx
@@ -1,13 +1,37 @@
 import React from 'react'
-import { Button } from '@radix-ui/themes'
+import { Button, Table, TableCell, TableHeader } from '@radix-ui/themes'
 import Link from 'next/link'
-const BugsPage = () => {
+import prisma from '@/prisma/client'
+
+const BugsPage = async () => {
+    const bugs = await prisma.bug.findMany({})
+
     return (
         <div>
             <h1>Bugs page</h1>
-            <Button>
-                <Link href='/bugs/new'>Add Bug</Link>
-            </Button>
+            <div className='mb-5'>
+                <Button>
+                    <Link href='/bugs/new'>Add Bug</Link>
+                </Button>
+            </div>
+            <Table.Root variant='surface'>
+                <Table.Header>
+                    <Table.ColumnHeaderCell>Bug</Table.ColumnHeaderCell>
+                    <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
+                    <Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
+                </Table.Header>
+                <Table.Body>{bugs.map(bug => {
+                    return (
+                        <Table.Row key={bug.id}>
+                            <TableCell>
+                                {bug.title}
+                                <div className='block md:hidden'>{bug.status}</div>
+                            </TableCell>
+                            <TableCell className='hidden md:table-cell'>{bug.status}</TableCell>
+                            <TableCell className='hidden md:table-cell'>{bug.createdAt.toDateString()}</TableCell>
+                        </Table.Row>)
+                })}</Table.Body>
+            </Table.Root>
         </div>
     )
 }
