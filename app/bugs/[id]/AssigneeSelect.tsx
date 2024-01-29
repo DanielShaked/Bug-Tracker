41a8d816e0cca72eb472c1dbd1335153b589e1d9
DanielShaked <89588973+DanielShaked@users.noreply.github.com>
2024-01-29 20:16:48 +0200

    Populate select from api user

--- app/bugs/[id]/AssigneeSelect.tsx
+++ app/bugs/[id]/AssigneeSelect.tsx
@@ -1,13 +1,37 @@
 'use client'
+import { User } from '@prisma/client'
 import { Select } from '@radix-ui/themes'
-
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 const AssigneeSelect = () => {
+
+    const [users, setUsers] = useState<User[]>([])
+
+    useEffect(() => {
+        loadUsers()
+
+        return () => {
+
+        }
+    }, [])
+
+    const loadUsers = async () => {
+        const { data } = await axios.get<User[]>('/api/users')
+        setUsers(data)
+    }
+
+
     return (
         <Select.Root>
             <Select.Trigger placeholder='Assign...'></Select.Trigger>
             <Select.Content>
                 <Select.Group>
                     <Select.Label>Suggestions</Select.Label>
+                    {users && users.map(user => {
+                        return (
+                            <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>
+                        )
+                    })}
                     <Select.Item value='1'>Daniel Shaked</Select.Item>
                     <Select.Item value='1'>Yuval Hayoon</Select.Item>
                 </Select.Group>
