49e37d8c54f222be86f26db208b28cb670a14206
DanielShaked <89588973+DanielShaked@users.noreply.github.com>
2024-04-07 18:26:10 +0300

    Implement Pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
 import Pagination from './components/Pagination'
 
-export default function Home() {
+export default function Home({ searchParams }: { searchParams: { page: string } }) {
   return (
     <div>
-      <Pagination itemCount={20} pageSize={5} currentPage={2} />
+      <Pagination itemCount={20} pageSize={5} currentPage={+searchParams.page} />
     </div>
   )
 }
