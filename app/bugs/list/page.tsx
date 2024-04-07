ce7efdaf5d922ed9419eaf80ed6ba2c1b96b5a0b
DanielShaked <89588973+DanielShaked@users.noreply.github.com>
2024-04-07 17:25:46 +0300

    Make columns sortable

--- app/bugs/list/page.tsx
+++ app/bugs/list/page.tsx
@@ -2,13 +2,28 @@ import prisma from '@/prisma/client'
 import { Table, TableCell } from '@radix-ui/themes'
 import { BugStatusBadge, Link } from '@/app/components'
 import BugToolbar from './BugToolbar'
-import { Status } from '@prisma/client'
+import NextLink from 'next/link'
+import { Bug, Status } from '@prisma/client'
+import { ArrowUpIcon } from '@radix-ui/react-icons'
 
 interface Props {
-    searchParams: { status: Status }
+    searchParams: { status: Status, orderBy: keyof Bug }
 }
 
 const BugsPage = async ({ searchParams }: Props) => {
+
+
+    const columns: {
+        label: string;
+        value: keyof Bug;
+        className?: string;
+    }[] = [
+            { label: 'Bug', value: 'title' },
+            { label: 'Status', value: 'status', className: 'hidden md:table-cell' },
+            { label: 'Created', value: 'createdAt', className: 'hidden md:table-cell' },
+        ]
+
+
     const statuses = Object.values(Status)
     console.log('statuses', statuses)
     const status = statuses.includes(searchParams.status) ? searchParams.status : undefined
@@ -23,9 +38,14 @@ const BugsPage = async ({ searchParams }: Props) => {
             <Table.Root variant='surface'>
                 <Table.Header>
                     <Table.Row>
-                        <Table.ColumnHeaderCell>Bug</Table.ColumnHeaderCell>
-                        <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
-                        <Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
+                        {columns.map((column) =>
+                            <Table.ColumnHeaderCell key={column.value}>
+                                <NextLink href={{
+                                    query: { ...searchParams, orderBy: column.value }
+                                }}>{column.label}</NextLink>
+                                {column.value === searchParams.orderBy && <ArrowUpIcon className='inline' />}
+                            </Table.ColumnHeaderCell>
+                        )}
                     </Table.Row>
                 </Table.Header>
                 <Table.Body>{bugs.map(bug => {
