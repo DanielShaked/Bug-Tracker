79491bd5ddaa923dd867d7997c4cf38c41bbbc7a
DanielShaked <89588973+DanielShaked@users.noreply.github.com>
2024-04-10 10:58:32 +0300

    Layout the Dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,20 @@ import LatestBugs from './LatestBugs'
 import BugSummery from './BugSummery'
 import Prisma from '@/prisma/client'
 import BugChart from './BugChart'
+import { Flex, Grid } from '@radix-ui/themes'
 
 export default async function Home() {
   const open = await Prisma.bug.count({ where: { status: 'OPEN' } })
   const inProgress = await Prisma.bug.count({ where: { status: 'IN_PROGRESS' } })
   const closed = await Prisma.bug.count({ where: { status: 'CLOSED' } })
+
   return (
-    <div>
+    <Grid columns={{ initial: '1', md: '2' }} gap='5'>
+      <Flex direction='column' gap='5'>
+        <BugSummery open={open} inProgress={inProgress} closed={closed} />
+        <BugChart open={open} inProgress={inProgress} closed={closed} />
+      </Flex>
       <LatestBugs />
-      <BugSummery open={open} inProgress={inProgress} closed={closed} />
-      <BugChart open={open} inProgress={inProgress} closed={closed} />
-    </div>
+    </Grid>
   )
 }
