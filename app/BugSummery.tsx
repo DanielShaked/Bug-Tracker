34fe8e8804dc807ea87b625d6c0555938c5c9916
DanielShaked <89588973+DanielShaked@users.noreply.github.com>
2024-04-10 10:46:24 +0300

    Build BugSummery cmp

---
+++ app/BugSummery.tsx
@@ -0,0 +1,37 @@
+import { Status } from '@prisma/client'
+import { Card, Flex, Text } from '@radix-ui/themes'
+import Link from 'next/link'
+import React from 'react'
+
+interface Props {
+    open: number,
+    inProgress: number,
+    closed: number
+}
+const BugSummery = ({ open, inProgress, closed }: Props) => {
+    const containers: {
+        label: string,
+        value: number,
+        status: Status
+    }[] = [
+            { label: 'Open Bugs', value: open, status: 'OPEN' },
+            { label: 'In-progress Bugs', value: inProgress, status: 'IN_PROGRESS' },
+            { label: 'Closed Bugs', value: closed, status: 'CLOSED' },
+        ]
+
+    return (
+        <Flex gap='4'>
+            {containers.map(container => {
+                return <Card key={container.label}>
+                    <Flex direction='column' gap='1'>
+                        <Link className='text-sm font-medium' href={`/bug/list?status=${container.value}`}>{container.label}</Link>
+                        <Text size='5' className='font-bold'>{container.value}</Text>
+                    </Flex>
+                </Card>
+            })}
+
+        </Flex>
+    )
+}
+
+export default BugSummery
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import Image from 'next/image'
 import Pagination from './components/Pagination'
 import LatestBugs from './LatestBugs'
+import BugSummery from './BugSummery'
+import Prisma from '@/prisma/client'
 
-export default function Home() {
+export default async function Home() {
+  const open = await Prisma.bug.count({ where: { status: 'OPEN' } })
+  const inProgress = await Prisma.bug.count({ where: { status: 'IN_PROGRESS' } })
+  const closed = await Prisma.bug.count({ where: { status: 'CLOSED' } })
   return (
     <div>
       <LatestBugs />
+      <BugSummery open={open} inProgress={inProgress} closed={closed} />
     </div>
   )
 }
